a541cc74905fbfd25ad1949c22e4d2e51a61f21c
m-wells <m-wells@users.noreply.github.com>
2026-04-18 16:13:00 +0000

    deploy: 7cd5b3a72ee18491db0ee1b3fe16cc433cb77533

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cb30a210.js',
+                path_to_root + 'searchindex-c48aea07.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
